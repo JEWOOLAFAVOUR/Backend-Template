158b8a9685873c18178c1644394a9bcb4413211d
JEWOOLAFAVOUR <98407795+JEWOOLAFAVOUR@users.noreply.github.com>
2025-03-07 12:59:35 +0100

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/swep-project")
+mongoose.connect("mongodb://127.0.0.1:27017/backend-template")
     // mongoose.connect(process.env.MONGO_URL, {
     // useNewUrlParser: true,
     // useCreateIndex: true,
@@ -10,10 +10,3 @@ mongoose.connect("mongodb://127.0.0.1:27017/swep-project")
     // good
     .then(() => console.log('db is connected'))
     .catch((err) => console.log('db connection failed: ', err.message || err));
-
-// mailTransport().sendMail({
-//   from: 'emailverification@email.com',
-//   to: newUser.email,
-//   subject: "Verify your email account",
-//   html: generateEmailTemplate(OTP)
-// });
