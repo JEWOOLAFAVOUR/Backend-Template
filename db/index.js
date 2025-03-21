cf68ae9161349b615dda99d37f269103d977c9c3
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2025-03-21 15:36:28 +0100

    updated app.ts

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/backend-template")
+mongoose.connect("mongodb://127.0.0.1:27017/victoria")
     // mongoose.connect(process.env.MONGO_URL, {
     // useNewUrlParser: true,
     // useCreateIndex: true,
