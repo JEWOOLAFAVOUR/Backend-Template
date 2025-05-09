5e0cfe08868c09518a9b257d9d504a2201412596
JEWOOLAFAVOUR <98407795+JEWOOLAFAVOUR@users.noreply.github.com>
2025-05-09 11:10:38 +0100

    Update app.ts

--- app.ts
+++ app.ts
@@ -11,6 +11,7 @@ import fs from "fs";
 import path from "path";
 import { v4 as uuidv4 } from "uuid";
 import authRouter from "./routers/auth-route/userRoute";
+import "./db";
 
 const expressSanitizer = require("express-sanitizer");
 
